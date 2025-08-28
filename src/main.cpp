c22fb9de18b8b336986f4275969a6bf49209ce35
serpentarie <serpentariee@gmail.com>
2025-08-28 19:49:50 +0300

    consol multithread app

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,111 @@
 #include "logger.h"
+#include <iostream>
+#include <queue>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
+#include <thread>
+#include <sstream>
 
-int main()
+LogLevel parse_level(const std::string &level_str)
 {
-    Logger logger("app.log", LogLevel::WARNING);
+    if (level_str == "ERROR")
+        return LogLevel::ERROR;
+    if (level_str == "WARNING")
+        return LogLevel::WARNING;
+    if (level_str == "INFO")
+        return LogLevel::INFO;
+    return LogLevel::INFO;
+}
 
-    logger.log("Information", LogLevel::INFO); // не запишется
-    logger.log("Warning alarm", LogLevel::WARNING);
-    logger.log("UGH", LogLevel::ERROR);
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cerr << "Enter: " << argv[0] << " [filename] [default level]" << std::endl;
+        std::cerr << "Default level variety: ERROR, WARNING, INFO" << std::endl;
+        return 1;
+    }
 
-    logger.setLogLevel(LogLevel::INFO);
-    logger.log("Information", LogLevel::INFO); // теперь инфо пишется
+    std::string filename = argv[1];
+    std::string default_level_str = argv[2];
+    LogLevel default_level = parse_level(default_level_str);
+    if (default_level == LogLevel::INFO && default_level_str != "INFO")
+    {
+        std::cerr << "Invalid defult level: " << default_level_str << std::endl;
+    }
+
+    Logger logger(filename, default_level);
+
+    std::queue<std::pair<std::string, LogLevel>> log_queue;
+    std::mutex mut;
+    std::condition_variable cv;
+    std::atomic<bool> done(false);
+    std::thread worker([&]()
+                       {
+        while (true){
+            std::unique_lock<std::mutex> lk(mut);
+            cv.wait(lk, [&]{ return !log_queue.empty() || done.load();});
+            if (done.load() && log_queue.empty()) {
+                break;
+            }
+            auto item = log_queue.front();
+            log_queue.pop();
+            lk.unlock();
+            logger.log(item.first, item.second);
+        } });
+
+    std::cout << "Enter '[message] + [level of importance]' or 'exit' to exit" << std::endl;
+    while (true)
+    {
+        std::string input;
+        std::getline(std::cin, input);
+        if (input.empty())
+            continue;
+        if (input == "exit")
+            break;
+
+        std::istringstream iss(input);
+        std::string msg, token, posible_level;
+        while (iss >> token)
+        {
+            if (!posible_level.empty())
+            {
+                msg += " " + posible_level;
+            }
+            posible_level = token;
+        }
+        if (!msg.empty())
+        {
+            msg = msg.substr(1);
+        }
+        else
+        {
+            msg = posible_level;
+            posible_level.clear();
+        }
+
+        LogLevel level = default_level;
+        if (!posible_level.empty())
+        {
+            LogLevel parsed = parse_level(posible_level);
+            if (parsed != LogLevel::INFO || posible_level == "INFO")
+            {
+                level = parsed;
+            }
+            else
+            {
+                msg += " " + posible_level;
+            }
+        }
+
+        {
+            std::lock_guard<std::mutex> lk(mut);
+            log_queue.push({msg, level});
+        }
+        cv.notify_one();
+    }
+    done.store(true);
+    cv.notify_one();
+    worker.join();
 }
